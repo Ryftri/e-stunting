19cba4629f41629d11dd2e8f246228422411854b
Rayhan Zulfitri Dwi Cahyo <rayhanzulfitri@gmail.com>
2025-04-16 09:03:42 +0700

    turn off obfuscate

--- next-css-obfuscator.config.cjs
+++ next-css-obfuscator.config.cjs
@@ -1,14 +1,12 @@
 module.exports = {
-    enable: true,
-    mode: "random", // random | simplify | simplify-seedable
+    enable: false,
+    mode: "simplify", // random | simplify | simplify-seedable
     refreshClassConversionJson: false, // recommended set to true if not in production
+    content: ['./app/**/*.{js,ts,jsx,tsx}'],
     allowExtensions: [".jsx", ".tsx", ".js", ".ts", ".html", ".rsc"],
   
     blackListedFolderPaths: [
       "./.next/cache",
       /\.next\/server\/pages\/api/,
-      /_document..*js/,
-      /_app-.*/,
-      /__.*/, // <= maybe helpful if you are using Next.js Lcal Fonts [1*]
     ],
   };
